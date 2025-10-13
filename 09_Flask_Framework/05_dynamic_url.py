8afe7ab96a747ad2e21385fb39c5f05192d5a5da
Death gravity <deathgravity7@gmail.com>
2025-10-14 00:03:35 +0530

    Completed the flask along with web framework

--- 09_Flask_Framework/05_dynamic_url.py
+++ 09_Flask_Framework/05_dynamic_url.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template,request
+from flask import Flask , render_template,request,redirect,url_for
 # Hey Flask, create a new web app for me, and use this current Python file as the main module."
 app=Flask(__name__)
 
@@ -8,13 +8,6 @@ app=Flask(__name__)
 {% .. . % } condition for loops
 {# ... # this is for comments }
 '''
-
-
-@app.route("/")
-def welcome():
-    return render_template("jinja_.html")
-
-
 @app.route("/form",methods=["GET","POST"])
 def form():
     if request.method=="POST":
@@ -23,8 +16,14 @@ def form():
     return render_template("form.html")
 
 
+
 # The approute decorator tells the flask when someone visit
 #  this url  it teel to run this function 
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
 # if condition 
 @app.route("/successif/<int:score>") 
 def new_value(score):
@@ -32,7 +31,27 @@ def new_value(score):
     return render_template("new_result.html", results=score)
 
 
+## creating fail url 
+@app.route("/fail/<int>:score>")
+def fail(score):
 
+    return render_template("new_result.html",results=score)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        science = float(request.form["science"])
+        maths = float(request.form["maths"])
+        c = float(request.form["c"])
+        data_science = float(request.form["datascience"])
+
+        total_score = (science + maths + c + data_science) / 4
+        return redirect(url_for("successess", score=total_score))
+
+
+
+
+#variable rule
 @app.route("/successess/<int:score>") 
 def successess(score):
 
